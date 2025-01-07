39811f4128c8b1ef49cea8e7ae494a22b0e4f54b
voaidesr <voaides.r@yahoo.com>
2025-01-07 18:56:21 +0200

    added planetarium

---
+++ src/html/dist/index.aa4b499a.js
@@ -0,0 +1,8 @@
+function scrollToNextSlide(nextSlideId) {
+    const nextSlide = document.querySelector(`#${nextSlideId}`);
+    if (nextSlide) nextSlide.scrollIntoView({
+        behavior: 'smooth'
+    });
+}
+
+//# sourceMappingURL=index.aa4b499a.js.map
